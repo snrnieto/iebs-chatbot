24a87a0b95e7d466890f6779d4f3dcec47386560
Sebastian Nieto <snrnieto1999@hotmail.com>
2024-03-26 17:04:32 -0500

    [ADD] update helperText

--- src/app/text.ts
+++ src/app/text.ts
@@ -4,9 +4,11 @@ Tradicionalmente, las soluciones a esta problemática han implicado ofrecer plaz
 
 En este contexto, este proyecto ha propuesto una solución innovadora: un modelo que permite a la sociedad colaborar en la compra de apartamentos para alojar temporalmente a familias refugiadas, sin necesidad de donaciones directas. Este modelo se basa en la tecnología blockchain para garantizar transparencia y gestión eficiente de contribuciones y recompensas a través de tokens. Los colaboradores colocan una cantidad mínima reembolsable, que se devuelve tras un período de alojamiento, generando así un ciclo sostenible de apoyo.
 
-El proceso para convertirse en colaborador es simple: cualquier persona interesada puede participar colocando una cantidad mínima reembolsable en la plataforma RefuToken. Esta cantidad se utiliza para adquirir y los inmuebles destinados a las familias refugiadas. La selección de las familias beneficiarias se realiza de manera justa y transparente, priorizando las áreas con mayor necesidad identificadas a través de datos socioeconómicos y análisis de necesidades. No hay restricciones en cuanto a la nacionalidad o el estatus de refugiado de las familias que pueden beneficiarse del proyecto, siempre y cuando cumplan con los criterios establecidos.
+El proceso para convertirse en colaborador es simple: cualquier persona interesada puede participar colocando una cantidad mínima reembolsable ( esta cantidad puede ser visualizada en el sitio web, este puede variar dependiendo del momento) en la plataforma RefuToken. Esta cantidad se utiliza para adquirir y los inmuebles destinados a las familias refugiadas. La selección de las familias beneficiarias se realiza de manera justa y transparente, priorizando las áreas con mayor necesidad identificadas a través de datos socioeconómicos y análisis de necesidades. No hay restricciones en cuanto a la nacionalidad o el estatus de refugiado de las familias que pueden beneficiarse del proyecto, siempre y cuando cumplan con los criterios establecidos.
 
 
 Responde de manera corta(menos de 100 palabras) , concisa y amable la siguiente pregunta con respecto a la información anterior, sería ideal que solo tuvieras en cuenta la información anterior, si no sabes la respuesta o la pregunta no está relacionada con la información anterior, puedes decir "No sé la respuesta" o "No entiendo la pregunta".
 
-Pregunta del usuario:`;
+Pregunta del usuario:
+
+`;
